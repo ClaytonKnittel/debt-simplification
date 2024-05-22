fc60267f9ecba01cd109d1a1552799a9f4630459
Clayton Knittel <cknit1999@gmail.com>
2024-05-22 16:30:31 -0700

    debt graph impl beginning

--- src/debt_graph.cc
+++ src/debt_graph.cc
@@ -1,1 +1,7 @@
+#include "src/debt_graph.h"
 
+DebtGraphNode::DebtGraphNode(uint64_t id) : id_(id) {}
+
+void DebtGraphNode::AddDebt(uint64_t owee_id, Cents amount) {
+  debts_[owee_id] += amount;
+}
--- src/debt_graph.h
+++ src/debt_graph.h
@@ -1,8 +1,28 @@
 #pragma once
 
+#include <string>
+
 #include "absl/container/flat_hash_map.h"
+
+typedef uint64_t Cents;
+
+class DebtGraphNode {
+ public:
+  explicit DebtGraphNode(uint64_t id);
+
+  void AddDebt(uint64_t owee_id, Cents amount);
+
+ private:
+  uint64_t id_;
+  absl::flat_hash_map<uint64_t, Cents> debts_;
+};
 
 class DebtGraph {
  private:
-  absl::flat_hash_map<int, int> guys;
+  // Map of user names to unique id's.
+  absl::flat_hash_map<std::string, uint64_t> id_map_;
+
+  // List of all nodes of the graph. A user's id is the index into this list
+  // where their corresponding node is.
+  std::vector<DebtGraphNode> node_list_;
 };
