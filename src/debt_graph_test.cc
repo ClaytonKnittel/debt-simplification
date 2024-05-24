1c41918bf386f36dd14e79e5d6b8c7a13579436d
Clayton Knittel <cknit1999@gmail.com>
2024-05-23 20:34:58 -0700

    more tests

--- src/debt_graph_test.cc
+++ src/debt_graph_test.cc
@@ -1,5 +1,7 @@
 #include "src/debt_graph.h"
 
+#include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
 #include "google/protobuf/text_format.h"
 #include "gtest/gtest.h"
 
@@ -10,22 +12,77 @@ namespace debt_simpl {
 using google::protobuf::TextFormat;
 using ::testing::Eq;
 
-TEST(TestDebtGraph, TestSimple) {
-  DebtList debt_list;
-  ASSERT_TRUE(TextFormat::ParseFromString(R"(
-    transactions {
-      lender: "alice"
-      receiver: "bob"
-      cents: 100
-    })",
-                                          &debt_list));
+class TestDebtGraph : public ::testing::Test {
+ protected:
+  absl::StatusOr<DebtGraph> CreateFromString(
+      absl::string_view debt_list_proto) {
+    DebtList debt_list;
+    if (!TextFormat::ParseFromString(debt_list_proto, &debt_list)) {
+      return absl::InternalError(
+          absl::StrFormat("Failed to construct DebtList proto from string %s",
+                          debt_list_proto));
+    }
 
-  auto status = DebtGraph::BuildFromProto(debt_list);
+    return DebtGraph::BuildFromProto(debt_list);
+  }
+};
+
+TEST_F(TestDebtGraph, SingleTransaction) {
+  auto status = CreateFromString(
+      R"(
+      transactions {
+        lender: "alice"
+        receiver: "bob"
+        cents: 100
+      }
+      )");
   ASSERT_TRUE(status.ok());
-  DebtGraph& graph = status.value();
+  const DebtGraph& graph = status.value();
 
   EXPECT_THAT(graph.AmountOwed("alice", "bob"), IsOkAndHolds(Eq(100)));
   EXPECT_THAT(graph.AmountOwed("bob", "alice"), IsOkAndHolds(Eq(-100)));
+}
+
+TEST_F(TestDebtGraph, DebtsCombine) {
+  auto status = CreateFromString(
+      R"(
+      transactions {
+        lender: "alice"
+        receiver: "bob"
+        cents: 1000
+      }
+      transactions {
+        lender: "alice"
+        receiver: "bob"
+        cents: 500
+      }
+      )");
+  ASSERT_TRUE(status.ok());
+  const DebtGraph& graph = status.value();
+
+  EXPECT_THAT(graph.AmountOwed("alice", "bob"), IsOkAndHolds(Eq(1500)));
+  EXPECT_THAT(graph.AmountOwed("bob", "alice"), IsOkAndHolds(Eq(-1500)));
+}
+
+TEST_F(TestDebtGraph, DebtAndCreditCancel) {
+  auto status = CreateFromString(
+      R"(
+      transactions {
+        lender: "alice"
+        receiver: "bob"
+        cents: 200
+      }
+      transactions {
+        lender: "bob"
+        receiver: "alice"
+        cents: 150
+      }
+      )");
+  ASSERT_TRUE(status.ok());
+  const DebtGraph& graph = status.value();
+
+  EXPECT_THAT(graph.AmountOwed("alice", "bob"), IsOkAndHolds(Eq(50)));
+  EXPECT_THAT(graph.AmountOwed("bob", "alice"), IsOkAndHolds(Eq(-50)));
 }
 
 }  // namespace debt_simpl
--- upload.sh
+++ upload.sh
@@ -3,15 +3,15 @@
 sftp -oPort=5167 cknittel@cknittel.com:Documents/VSCode/debt-simplification <<EOF
 put src/debt_graph.h src/debt_graph.h
 put src/debt_graph.cc src/debt_graph.cc
+put src/debt_graph_test.cc src/debt_graph_test.cc
 put src/debts.proto src/debts.proto
 put src/utils.h src/utils.h
 put src/BUILD src/BUILD
 
--clang-format -i src/debt_graph.h
--clang-format -i src/debt_graph.cc
-!ssh -p 5167 cknittel@cknittel.com "cd Documents/VSCode/debt-simplification && clang-format src/debt_graph.h -i && clang-format src/debt_graph.cc -i && clang-format src/utils.h -i && bazel build --noshow_progress --copt="-Wno-unknown-warning-option" //src:main"
+!ssh -p 5167 cknittel@cknittel.com "cd Documents/VSCode/debt-simplification && clang-format src/debt_graph.h -i && clang-format src/debt_graph.cc -i && clang-format src/debt_graph_test.cc -i && clang-format src/utils.h -i && bazel build --noshow_progress --copt="-Wno-unknown-warning-option" --copt="-Wno-c++17-extensions" //src/... && bazel test --noshow_progress --copt="-Wno-unknown-warning-option" --copt="-Wno-c++17-extensions" //src/..."
 
 get src/debt_graph.h src/debt_graph.h
 get src/debt_graph.cc src/debt_graph.cc
+get src/debt_graph_test.cc src/debt_graph_test.cc
 get src/utils.h src/utils.h
 EOF
