f792c6943c964f0ff02c8ce9696bd071a66f793e
Clayton Knittel <cknit1999@gmail.com>
2024-05-24 18:47:05 -0700

    cpp toolchain

--- src/expense_simplifier.cc
+++ src/expense_simplifier.cc
@@ -1,5 +1,8 @@
 #include "src/expense_simplifier.h"
 
+#include <cstdint>
+#include <deque>
+#include <optional>
 #include <vector>
 
 #include "src/debt_graph.h"
@@ -9,8 +12,49 @@ namespace debt_simpl {
 ExpenseSimplifier::ExpenseSimplifier(DebtGraph&& graph)
     : graph_(std::move(graph)) {}
 
-DebtGraph ExpenseSimplifier::MinimalTransactions() const {
-  return DebtGraph();
+const DebtGraph& ExpenseSimplifier::MinimalTransactions() const {
+  return simplified_expenses_;
+}
+
+std::vector<LayeredGraphNode> ExpenseSimplifier::ConstructLayeredGraph(
+    uint64_t source, uint64_t sink) const {
+  std::vector<LayeredGraphNode> layered_graph;
+  std::deque<std::pair<uint64_t, uint32_t>> id_q;
+  id_q.push_back({ source, 0 });
+  uint32_t sink_dist = UINT32_MAX;
+  // A map from node id's to the index in `layered_graph` of the head of the
+  // node.
+  absl::flat_hash_map<uint64_t, std::optional<uint64_t>> visited_nodes;
+
+  while (!id_q.empty()) {
+    const auto [node_id, depth] = id_q.front();
+    id_q.pop_front();
+
+    if (depth > sink_dist) {
+      // Stop once we have passed the sink.
+      break;
+    } else if (node_id == sink) {
+      sink_dist = depth;
+    }
+
+    layered_graph.push_back(
+        LayeredGraphNode{ .type = LayeredGraphNodeType::Head,
+                          .head = { .id = node_id, .level = depth } });
+    for (const auto& [neighbor_id, capacity] : graph_.AllDebts(node_id)) {
+      if (capacity == 0) {
+        continue;
+      }
+
+      layered_graph.push_back(LayeredGraphNode{
+          .type = LayeredGraphNodeType::Neighbor,
+          .neighbor = { .id = neighbor_id, .capacity = capacity } });
+    }
+
+    // If we did not add any edges from this node, remove it from the graph.
+    if (layered_graph.back().type == LayeredGraphNodeType::Head) {
+      layered_graph.pop_back();
+    }
+  }
 }
 
 }  // namespace debt_simpl
--- src/expense_simplifier.h
+++ src/expense_simplifier.h
@@ -7,13 +7,52 @@
 
 namespace debt_simpl {
 
+enum class LayeredGraphNodeType {
+  Head,
+  Neighbor,
+};
+
+struct LayeredGraphNode {
+  LayeredGraphNodeType type;
+
+  union {
+    // Members are defined if `type` == Head.
+    struct {
+      // The user id for this node. The subsequent nodes of type `Neighbor` are
+      // the neighbors of this user.
+      uint64_t id;
+
+      // The level of this node, i.e. the distance between this node and the
+      // source node. This == 0 for the source node.
+      uint32_t level;
+    } head;
+
+    // Members are defined if `type` == Neighbor.
+    struct {
+      // The id of one of the neighbors this user is connected to.
+      uint64_t id;
+
+      // The available capacity for flow of money from this user to the
+      // neighbor.
+      Cents capacity;
+    } neighbor;
+  };
+};
+
 class ExpenseSimplifier {
  public:
   explicit ExpenseSimplifier(DebtGraph&& graph);
 
-  DebtGraph MinimalTransactions() const;
+  const DebtGraph& MinimalTransactions() const;
 
  private:
+  // Constructs a layered graph from `source` to `sink` using only edges on the
+  // shortest paths from `source` to `sink` in `graph_`.
+  std::vector<LayeredGraphNode> ConstructLayeredGraph(uint64_t source,
+                                                      uint64_t sink) const;
+
+  DebtGraph simplified_expenses_;
+
   AugmentedDebtGraph graph_;
 };
 
