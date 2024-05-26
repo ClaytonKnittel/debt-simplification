649e91d1494e7c5801927444c4832c9c3229b7ab
Clayton Knittel <cknit1999@gmail.com>
2024-05-26 01:50:48 -0700

    harder expense simpl test

--- src/expense_simplifier_test.cc
+++ src/expense_simplifier_test.cc
@@ -1,6 +1,7 @@
 #include "src/expense_simplifier.h"
 
 #include <iostream>
+#include <stdint.h>
 
 #include "gmock/gmock.h"
 #include "google/protobuf/text_format.h"
@@ -198,6 +199,145 @@ TEST_F(TestBlockingFlow, TestPrunePaths) {
   EXPECT_EQ(layered_graph[4],
             (LayeredGraphNode{ .type = LayeredGraphNodeType::Head,
                                .head = { .id = bob_id, .level = 2 } }));
+}
+
+TEST_F(TestBlockingFlow, TestMultipleFlowsPossible) {
+  // This is a visual representation of the flow network (from left to right):
+  //
+  //     _2_ b
+  //   /       \_1_
+  // a          _1_ e
+  //   \_3_   /       \_1_
+  //        c          _9_ f
+  //          \_1_   /
+  //               d
+  ASSERT_OK_AND_DEFINE(DebtGraph, graph, CreateFromString(R"(
+    transactions {
+      lender: "b"
+      receiver: "a"
+      cents: 2
+    }
+    transactions {
+      lender: "c"
+      receiver: "a"
+      cents: 3
+    }
+    transactions {
+      lender: "d"
+      receiver: "c"
+      cents: 1
+    }
+    transactions {
+      lender: "e"
+      receiver: "c"
+      cents: 1
+    }
+    transactions {
+      lender: "e"
+      receiver: "b"
+      cents: 1
+    }
+    transactions {
+      lender: "f"
+      receiver: "d"
+      cents: 9
+    }
+    transactions {
+      lender: "f"
+      receiver: "e"
+      cents: 1
+    })"));
+
+  ASSERT_OK_AND_DEFINE(uint64_t, a_id, graph.FindUserId("a"));
+  ASSERT_OK_AND_DEFINE(uint64_t, b_id, graph.FindUserId("b"));
+  ASSERT_OK_AND_DEFINE(uint64_t, c_id, graph.FindUserId("c"));
+  ASSERT_OK_AND_DEFINE(uint64_t, d_id, graph.FindUserId("d"));
+  ASSERT_OK_AND_DEFINE(uint64_t, e_id, graph.FindUserId("e"));
+  ASSERT_OK_AND_DEFINE(uint64_t, f_id, graph.FindUserId("f"));
+
+  ExpenseSimplifier solver(std::move(graph));
+
+  const auto layered_graph = solver.ConstructBlockingFlow(a_id, f_id);
+  ASSERT_EQ(layered_graph.size(), 3 + 2 + 3 + 2 + 2 + 1);
+
+  uint64_t a_idx = UINT64_MAX, b_idx = UINT64_MAX, c_idx = UINT64_MAX,
+           d_idx = UINT64_MAX, e_idx = UINT64_MAX, f_idx = UINT64_MAX;
+  for (uint64_t i = 0; i < layered_graph.size(); i++) {
+    if (layered_graph[i].type == LayeredGraphNodeType::Head) {
+      if (layered_graph[i].head.id == a_id) {
+        a_idx = i;
+      } else if (layered_graph[i].head.id == b_id) {
+        b_idx = i;
+      } else if (layered_graph[i].head.id == c_id) {
+        c_idx = i;
+      } else if (layered_graph[i].head.id == d_id) {
+        d_idx = i;
+      } else if (layered_graph[i].head.id == e_id) {
+        e_idx = i;
+      } else if (layered_graph[i].head.id == f_id) {
+        f_idx = i;
+      }
+    }
+  }
+  ASSERT_NE(a_idx, UINT64_MAX);
+  ASSERT_NE(b_idx, UINT64_MAX);
+  ASSERT_NE(c_idx, UINT64_MAX);
+  ASSERT_NE(d_idx, UINT64_MAX);
+  ASSERT_NE(e_idx, UINT64_MAX);
+  ASSERT_NE(f_idx, UINT64_MAX);
+
+  uint64_t c_to_d_edge_idx = UINT64_MAX;
+  for (uint64_t i = c_idx + 1;
+       layered_graph[i].type != LayeredGraphNodeType::Head; i++) {
+    if (layered_graph[i].neighbor.neighbor_head_idx == d_idx) {
+      c_to_d_edge_idx = i;
+      break;
+    }
+  }
+  ASSERT_NE(c_to_d_edge_idx, UINT64_MAX);
+
+  EXPECT_EQ(layered_graph[a_idx],
+            (LayeredGraphNode{ .type = LayeredGraphNodeType::Head,
+                               .head = { .id = a_id, .level = 0 } }));
+  EXPECT_EQ(layered_graph[a_idx + 1].type, LayeredGraphNodeType::Neighbor);
+  EXPECT_EQ(layered_graph[a_idx + 2].type, LayeredGraphNodeType::Neighbor);
+
+  // There are two possible flow networks:
+  //     _1_ b
+  //   /       \_1_
+  // a          _0_ e
+  //   \_1_   /       \_1_
+  //        c          _1_ f
+  //          \_1_   /
+  //               d
+  //
+  // and
+  //     _0_ b
+  //   /       \_0_
+  // a          _1_ e
+  //   \_2_   /       \_1_
+  //        c          _1_ f
+  //          \_1_   /
+  //               d
+  //
+  // Regardless which one is chosen, the following are true:
+
+  // The flow out of a is 2:
+  EXPECT_EQ(layered_graph[a_idx + 1].neighbor.flow +
+                layered_graph[a_idx + 2].neighbor.flow,
+            2);
+
+  // The flow through e is 1:
+  EXPECT_EQ(layered_graph[e_idx + 1].neighbor.flow, 1);
+
+  // The flow from c to d is 1:
+  EXPECT_EQ(layered_graph[c_to_d_edge_idx].neighbor.flow, 1);
+
+  // The flow into f is 2:
+  EXPECT_EQ(layered_graph[d_idx + 1].neighbor.neighbor_head_idx, f_idx);
+  EXPECT_EQ(layered_graph[d_idx + 1].neighbor.flow, 1);
+  EXPECT_EQ(layered_graph[e_idx + 1].neighbor.neighbor_head_idx, f_idx);
+  EXPECT_EQ(layered_graph[e_idx + 1].neighbor.flow, 1);
 }
 
 }  // namespace debt_simpl
--- upload.sh
+++ upload.sh
@@ -4,14 +4,29 @@ sftp -oPort=5167 cknittel@cknittel.com:Documents/VSCode/debt-simplification <<EO
 put src/debt_graph.h src/debt_graph.h
 put src/debt_graph.cc src/debt_graph.cc
 put src/debt_graph_test.cc src/debt_graph_test.cc
+put src/expense_simplifier.h src/expense_simplifier.h
+put src/expense_simplifier.cc src/expense_simplifier.cc
+put src/expense_simplifier_test.cc src/expense_simplifier_test.cc
 put src/debts.proto src/debts.proto
 put src/utils.h src/utils.h
 put src/BUILD src/BUILD
 
-!ssh -p 5167 cknittel@cknittel.com "cd Documents/VSCode/debt-simplification && clang-format src/debt_graph.h -i && clang-format src/debt_graph.cc -i && clang-format src/debt_graph_test.cc -i && clang-format src/utils.h -i && bazel build --noshow_progress --copt="-Wno-unknown-warning-option" --copt="-Wno-c++17-extensions" //src/... && bazel test --noshow_progress --copt="-Wno-unknown-warning-option" --copt="-Wno-c++17-extensions" //src/..."
+!ssh -p 5167 cknittel@cknittel.com "cd Documents/VSCode/debt-simplification && \
+  clang-format src/debt_graph.h -i && \
+  clang-format src/debt_graph.cc -i && \
+  clang-format src/debt_graph_test.cc -i && \
+  clang-format src/expense_simplifier.h -i && \
+  clang-format src/expense_simplifier.cc -i && \
+  clang-format src/expense_simplifier_test.cc -i && \
+  clang-format src/utils.h -i && \
+  bazel build --noshow_progress //src/... && \
+  bazel test --noshow_progress //src/..."
 
 get src/debt_graph.h src/debt_graph.h
 get src/debt_graph.cc src/debt_graph.cc
 get src/debt_graph_test.cc src/debt_graph_test.cc
+get src/expense_simplifier.h src/expense_simplifier.h
+get src/expense_simplifier.cc src/expense_simplifier.cc
+get src/expense_simplifier_test.cc src/expense_simplifier_test.cc
 get src/utils.h src/utils.h
 EOF
