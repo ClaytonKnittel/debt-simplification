8b0b31c49dafa67a3d39a7983149db0a861d8a1b
Clayton Knittel <cknit1999@gmail.com>
2024-05-27 05:11:20 -0700

    update upload.sh

--- server/upload.sh
+++ server/upload.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
-sftp -oPort=5167 cknittel@cknittel.com:Documents/VSCode/debt-simplification <<EOF
-put src/debt_graph.h src/debt_graph.h
-put src/debt_graph.cc src/debt_graph.cc
-put src/debt_graph_test.cc src/debt_graph_test.cc
-put src/expense_simplifier.h src/expense_simplifier.h
-put src/expense_simplifier.cc src/expense_simplifier.cc
-put src/expense_simplifier_test.cc src/expense_simplifier_test.cc
-put src/debts.proto src/debts.proto
-put src/utils.h src/utils.h
-put src/BUILD src/BUILD
+sftp -oPort=5167 cknittel@cknittel.com:Documents/VSCode/debt-simplification/server <<EOF
+put src/expense_simplifier/debt_graph.h src/expense_simplifier/debt_graph.h
+put src/expense_simplifier/debt_graph.cc src/expense_simplifier/debt_graph.cc
+put src/expense_simplifier/debt_graph_test.cc src/expense_simplifier/debt_graph_test.cc
+put src/expense_simplifier/expense_simplifier.h src/expense_simplifier/expense_simplifier.h
+put src/expense_simplifier/expense_simplifier.cc src/expense_simplifier/expense_simplifier.cc
+put src/expense_simplifier/expense_simplifier_test.cc src/expense_simplifier/expense_simplifier_test.cc
+put src/expense_simplifier/debts.proto src/expense_simplifier/debts.proto
+put src/expense_simplifier/utils.h src/expense_simplifier/utils.h
+put src/expense_simplifier/BUILD src/expense_simplifier/BUILD
 
-!ssh -p 5167 cknittel@cknittel.com "cd Documents/VSCode/debt-simplification && \
-  clang-format src/debt_graph.h -i && \
-  clang-format src/debt_graph.cc -i && \
-  clang-format src/debt_graph_test.cc -i && \
-  clang-format src/expense_simplifier.h -i && \
-  clang-format src/expense_simplifier.cc -i && \
-  clang-format src/expense_simplifier_test.cc -i && \
-  clang-format src/utils.h -i && \
+!ssh -p 5167 cknittel@cknittel.com "cd Documents/VSCode/debt-simplification/server && \
+  clang-format src/expense_simplifier/debt_graph.h -i && \
+  clang-format src/expense_simplifier/debt_graph.cc -i && \
+  clang-format src/expense_simplifier/debt_graph_test.cc -i && \
+  clang-format src/expense_simplifier/expense_simplifier.h -i && \
+  clang-format src/expense_simplifier/expense_simplifier.cc -i && \
+  clang-format src/expense_simplifier/expense_simplifier_test.cc -i && \
+  clang-format src/expense_simplifier/utils.h -i && \
   bazel build --noshow_progress //src/... && \
   bazel test --noshow_progress //src/..."
 
-get src/debt_graph.h src/debt_graph.h
-get src/debt_graph.cc src/debt_graph.cc
-get src/debt_graph_test.cc src/debt_graph_test.cc
-get src/expense_simplifier.h src/expense_simplifier.h
-get src/expense_simplifier.cc src/expense_simplifier.cc
-get src/expense_simplifier_test.cc src/expense_simplifier_test.cc
-get src/utils.h src/utils.h
+get src/expense_simplifier/debt_graph.h src/expense_simplifier/debt_graph.h
+get src/expense_simplifier/debt_graph.cc src/expense_simplifier/debt_graph.cc
+get src/expense_simplifier/debt_graph_test.cc src/expense_simplifier/debt_graph_test.cc
+get src/expense_simplifier/expense_simplifier.h src/expense_simplifier/expense_simplifier.h
+get src/expense_simplifier/expense_simplifier.cc src/expense_simplifier/expense_simplifier.cc
+get src/expense_simplifier/expense_simplifier_test.cc src/expense_simplifier/expense_simplifier_test.cc
+get src/expense_simplifier/utils.h src/expense_simplifier/utils.h
 EOF
