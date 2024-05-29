fbdc02856dd8893df9370dbe26af0932a10c1167
Clayton Knittel <cknit1999@gmail.com>
2024-05-28 18:02:43 -0700

    grpc types, but client methods not recognized

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React from 'react';
+
+import{DebtSimplifierClient} from 'proto/service_grpc_web_pb';
+import from 'proto/service_pb';
 
 export function App() {
+  const client = new DebtSimplifierClient('cknittel.com:3001', null, null);
+  const req = new proto.debt_simpl.TestReq();
+  req.setMsg('test guy');
+  client.test(
+      req, null, (err, res) = > {
+        console.log(`Got response : $ { res.msg() }`);
+      });
   return <></>;
 }
